5914626dc25d1143308b493dee1733dc2b395066
alphacpc <alphacpc@gmail.com>
2022-02-18 18:29:33 +0000

    Added GET and POST user

--- server.js
+++ server.js
@@ -2,13 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./Config/db_connect");
 require("dotenv").config();
+const userRouter = require("./Routers/user");
+
+// Define some constant
 const app = express()
 const PORT = 5050 || process.env.PORT;
+
+
+// Use middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/api/users", userRouter);
+
 
 
 
 // START CONNEXION FROM DB
 connectDB();
+
+
 
 
 //Launch connexion and open server
