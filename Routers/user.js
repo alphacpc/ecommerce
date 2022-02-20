ac9914d8adba229d1848ee4452d97eeb04200f46
alphacpc <alphacpc@gmail.com>
2022-02-20 01:13:08 +0000

    started config admin get

--- Routers/user.js
+++ Routers/user.js
@@ -1,8 +1,10 @@
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
+
+// UPDATE USER
 router.put("/:id", verifyTokenAndAuthorization , async (req,res) => {
 
     if(req.body.password){
@@ -24,10 +26,62 @@ router.put("/:id", verifyTokenAndAuthorization , async (req,res) => {
     }catch(err){
         res.status(500).json(err);
     }
-
-    
-
 })
+
+// DELETE USER
+router.delete("/:id", verifyTokenAndAuthorization, async(req,res) =>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...")
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+// GET USER
+router.get ("/find/:id", verifyTokenAndAdmin, async(req,res) =>{
+    try{
+        const user = await User.findById(req.params.id);
+        const {userPw, ...others} = user._doc
+        res.status(200).json(others)
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+// GET ALL USERS
+router.get("/", verifyTokenAndAdmin, async(req,res) =>{
+    
+    const query = req.query.new
+
+    try{
+        const users = query ? await User.find().sort({__id:-1}).limit(5) : await User.find();
+        res.status(200).json(users )
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+// GET USERS STATS
+router.get("/", verifyTokenAndAdmin, async(req,res) =>{
+    
+    const query = req.query.new
+
+    try{
+        const users = query ? await User.find().sort({__id:-1}).limit(5) : await User.find();
+        res.status(200).json(users )
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
 
 
 module.exports = router
