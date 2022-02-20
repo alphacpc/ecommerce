f7f3a4d579566bb2a3079cd3dfb29413df269392
alphacpc <alphacpc@gmail.com>
2022-02-20 00:47:35 +0000

    Added token for updated user

--- Routers/user.js
+++ Routers/user.js
@@ -3,10 +3,9 @@ const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
 
-router.put("/", verifyTokenAndAuthorization , async (req,res) => {
+router.put("/:id", verifyTokenAndAuthorization , async (req,res) => {
 
     if(req.body.password){
-
         const saltRounds = await bcrypt.genSalt(10);
         req.body.password  = await bcrypt.hash(req.body.psw, saltRounds);
     }
