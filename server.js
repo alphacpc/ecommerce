fc81b9ea94f5ba79ce2c4ac2323f144eeb7f2fae
alphacpc <alphacpc@gmail.com>
2022-02-22 20:30:18 +0000

    Probleme with CORS

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./Config/db_connect");
+const cors = require("cors");
 require("dotenv").config();
 const userRouter = require("./Routers/user");
 const authRouter = require("./Routers/auth");
 const productRouter = require("./Routers/product");
+const stripeRouter = require("./Routers/stripe");
 
 // Define some constant
 const app = express()
@@ -14,9 +16,11 @@ const PORT = 5050 || process.env.PORT;
 // Use middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/products", productRouter);
+app.use("/api/checkout", stripeRouter);
 
 
 // START CONNEXION FROM DB
