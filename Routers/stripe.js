a919d2cf51f1b2c966b27bb27dcf4a6eee33e43a
alphacpc <alphacpc@gmail.com>
2022-02-22 19:39:09 +0000

    Updated project frontend

--- Routers/stripe.js
+++ Routers/stripe.js
@@ -1,7 +1,21 @@
+// const { default: Stripe } = require("stripe")(process.env.STRIPE_KEY);
 const router = require("express").Router();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 
-router.post("/payment", (req; res) => {
-    stripe.
+router.post("/payment", (req, res) => {
+    
+    stripe.charges.create({
+        source: req.body.tokenId,
+        amount: req.body.amount,
+        currency: "usd",
+    }, (stripeErr, stripeRes) => {
+        if(stripeErr){
+            res.status(500).json(stripeErr)
+        }else{
+            res.status(200).json(stripeRes)
+        }
+    })
 })
+
+module.exports = router;
