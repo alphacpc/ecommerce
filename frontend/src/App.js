a31b8acf8f6572184aaa1d9a80a7cef251cd8dd2
alphacpc <alphacpc@gmail.com>
2022-03-02 16:51:41 +0000

    link css worked and change home page route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Pay from './Pay';
 import Success from './Success';
+import Home from './pages/Home';
+import "./assets/styles/App.css";
 
 
 function App() {
   return (
     <Router>
       <Switch>
+      <Route path="/" exact>
+          <Home/>
+        </Route> 
         <Route path="/pay">
           <Pay/>
         </Route>  
