c0d96dfd19e993e454edc6f3e6b88d44b3461e82
alphacpc <alphacpc@gmail.com>
2022-03-04 19:23:34 +0000

    Finish slider by added styled-components

---
+++ frontend/src/Datas.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id : 1,
+        title : "SUMMER HOLIDAYS",
+        desc : "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        img : require("./assets/images/model1.png"),
+        bg : "E83A14"
+    },
+    {
+        id : 2,
+        title : "SUMMER HOLIDAYS",
+        desc : "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        img : require("./assets/images/model2.png"),
+        bg : "AEA1BAC"
+    },
+    {
+        id : 3,
+        title : "SUMMER HOLIDAYS",
+        desc : "Lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        img : require("./assets/images/model3.png"),
+        bg : "1abc"
+    }
+]
--- frontend/src/components/Announcement.js
+++ frontend/src/components/Announcement.js
@@ -1,32 +1,66 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ArrowLeftOutlined, ArrowRightOutlined} from "@ant-design/icons";
 import { Link } from "react-router-dom"
 import ImageSlide from "./../assets/images/model1.png";
 import "./../assets/styles/Announcement.css"
+import {sliderItems} from "./../Datas"
+import styled from "styled-components";
 
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  background-color: #${(props) => props.bg};
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  transition: all 0.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
+`;
 
 const Announcement = () => {
-  return (
-    <div className="announcementContainer">
-        <div className="divArrow">
-            <ArrowLeftOutlined/>
-        </div>
 
-        <div className="divSlider">
-            <div className="imageSlider">
-                <img src={ImageSlide} alt="image du slide"/>
-            </div>
-            <div className="infoSlider">
-                <h2 className="titleSlide"></h2>
-                <p className="">Lorem ipsum</p>
-            </div>
-        </div>
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction) => {
+        
+        if (direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+        }
+    };
 
-        <div className="divArrow">
-            <ArrowRightOutlined/>
+    
+    return (
+        <div className="announcementContainer">
+            <div className="divArrow left" direction="left" onClick={() => handleClick("left")}>
+                <ArrowLeftOutlined/>
+            </div>
+            {/* <div className="wrapper" slideIndex={slideIndex} style={{"transform":`translateX(${(props) => props.slideIndex * -100}vw)`}}> */}
+            <Wrapper slideIndex={slideIndex}>
+                {sliderItems.map( (item) => (
+                    <Slide bg={item.bg} key={item.id}>
+                        <div className="imageSlider">
+                            <img src={item.img} alt="image du slide"/>
+                        </div>
+                        <div className="infoSlider">
+                            <h2 className="titleSlide">{item.title}</h2>
+                            <p className="">{item.desc}</p>
+                            <button>Voir plus</button>
+                        </div>
+                    </Slide>
+                ))}
+            </Wrapper>
+            {/* </div> */}
+
+            <div className="divArrow right" direction="right" onClick={() => handleClick("right")}>
+                <ArrowRightOutlined/>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Announcement
