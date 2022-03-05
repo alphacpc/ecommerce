a4d39f29bea9a14158392d34b7f3dbbbe160ef26
alphacpc <alphacpc@gmail.com>
2022-03-05 18:45:37 +0000

    updated footer component remove other color for item

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MediumOutlined, GithubOutlined, GitlabOutlined, LinkedinOutlined, YoutubeOutlined } from '@ant-design/icons';
+import { MediumOutlined, GithubOutlined, 
+  GitlabOutlined, LinkedinOutlined, 
+  YoutubeOutlined, PhoneOutlined,
+  MailOutlined, HomeOutlined, BuildOutlined } from '@ant-design/icons';
 import "./../assets/styles/Footer.css"
 
 const Footer = () => {
   return (
       <div className="footerContainer">
         <div className="footerItem">
-          <h1>bydoo.</h1>
+          <h1><Link to="/">bydoo.</Link></h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
             Adipisci reprehenderit ducimus nostrum corrupti officia 
             repellendus, rem rerum quisquam. Fugiat earum, assumenda 
             porro aut cupiditate perspiciatis sequi reiciendis 
             explicabo eum dolore?</p>
-            <div>
+            
+            <div className="methods-payement">
               <Link to="/">Visa</Link>
               <Link to="/">MasterCard</Link>
               <Link to="/">Express American</Link>
@@ -24,7 +28,7 @@ const Footer = () => {
         <div className="footerItem">
           <h2>Liens</h2>
 
-          <div>
+          <div className="column">
             <Link to="/">Mon compte</Link>
             <Link to="/">Boutique</Link>
             <Link to="/">Mon panier</Link>
@@ -33,8 +37,8 @@ const Footer = () => {
         </div>
 
         <div className="footerItem">
-          <h2>Reseaux sociau</h2>
-          <div>
+          <h2>Réseaux sociaux</h2>
+          <div className="column">
             <Link to="/">Linkedin <LinkedinOutlined/></Link>
             <Link to="/">Github <GithubOutlined/></Link>
             <Link to="/">Youtube <YoutubeOutlined/></Link>
@@ -45,10 +49,10 @@ const Footer = () => {
 
         <div className="footerItem">
           <h2>Contact</h2>
-          <div>
-            <p>bydoo@contact.com</p>
-            <p>+221 33 000 00 00</p>
-            <p>Yeumbeul Nord</p>
+          <div className="column">
+            <p><MailOutlined/> bydoo@contact.com</p>
+            <p><PhoneOutlined/> +221 33 000 00 00</p>
+            <p><HomeOutlined/> Yeumbeul Nord</p>
           </div>
         </div>
 
