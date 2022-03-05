36282a2126f9af4741564fdf271ec3b1f21c6db7
alphacpc <alphacpc@gmail.com>
2022-03-05 17:12:15 +0000

    updated best sell by customizer image watch and added overlay

--- frontend/src/Datas.js
+++ frontend/src/Datas.js
@@ -71,35 +71,56 @@ export const products = {
             id : 1,
             title : "Sacs",
             desc : "Lorem ipsum",
-            img : require("./assets/images/Produits/ban1.jpg"),
+            img : require("./assets/images/Produits/jacket2.png"),
             classname : "third"
         },
         {
             id : 2,
             title : "T-shirt",
             desc : "Lorem ipsum",
-            img : require("./assets/images/Produits/ban3.jpg"),
-            classname : "third"
+            img : require("./assets/images/Produits/montre2.png"),
+            classname : "montre"
         },
         {
             id : 3,
             title : "Montres",
             desc : "Lorem ipsum",
-            img : require("./assets/images/Produits/ban4.jpg"),
+            img : require("./assets/images/Produits/tshirt7.png"),
             classname : "third"
         },
         {
             id : 4,
             title : "Pull over",
             desc : "Lorem ipsum",
-            img : require("./assets/images/Produits/ban5.jpg"),
+            img : require("./assets/images/Produits/sac1.png"),
             classname : "half"
         },
         {
             id : 5,
             title : "Jeans",
             desc : "Lorem ipsum",
-            img : require("./assets/images/Produits/ban2.jpg"),
+            img : require("./assets/images/Produits/montre3.png"),
+            classname : "montre"
+        },
+        {
+            id : 5,
+            title : "Jeans",
+            desc : "Lorem ipsum",
+            img : require("./assets/images/Produits/jacket1.png"),
+            classname : "half"
+        },
+        {
+            id : 5,
+            title : "Jeans",
+            desc : "Lorem ipsum",
+            img : require("./assets/images/Produits/jacket6.png"),
+            classname : "half"
+        },
+        {
+            id : 5,
+            title : "Jeans",
+            desc : "Lorem ipsum",
+            img : require("./assets/images/Produits/tshirt2.png"),
             classname : "half"
         },
     ]
--- frontend/src/components/BestSell.js
+++ frontend/src/components/BestSell.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import "./../assets/styles/BestSell.css"
+import "./../assets/styles/BestSell.css";
+import { products } from "./../Datas";
+import {ShoppingOutlined, ShoppingCartOutlined, SearchOutlined, HeartOutlined} from "@ant-design/icons";
+
+
 
 const BestSell = () => {
+
+  const bestSell = products['best-sell'];
+
   return (
     <div className="bestContainer">
       <h2>Meilleures ventes</h2>
+      <div className="divBests">
+        {
+          bestSell.map((product, index) => (
+            <div className="bestItem">
+              <img className={`${product.classname}`} src={product.img} alt={`${product.title}`}/>
+              <div className="divIcons">
+                <SearchOutlined color=''/>
+                <HeartOutlined/>
+                <ShoppingCartOutlined/>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
